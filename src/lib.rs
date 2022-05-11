d45d05bce671815f6dfc4287053fd79142530ac3
nathan gd <nathan.gardet-derc@pm.me>
2022-05-11 15:36:42 +0200

    added storages

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
 
 #[cfg(test)]
@@ -19,7 +16,23 @@ pub use frame_support::{
     traits::Currency,
 };
 
+// pub use sp_runtime::traits::BlakeTwo256;
+
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+
 pub type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+#[derive(Clone, Encode, Decode, TypeInfo)]
+pub struct Dorg<AccountId> {
+    account: AccountId,
+    members: Vec<AccountId>,
+}
+
+#[derive(Clone, Encode, Decode, TypeInfo)]
+pub struct CallHash {
+    call_hash: (),
+}
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -27,28 +40,46 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		type Currency: Currency<Self::AccountId>;
-	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// the obiquitous event type
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        /// The trait to manage funds
+		type Currency: Currency<Self::AccountId>;
+	}
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::storage]
+	#[pallet::getter(fn dorg)]
+	pub type Dorgs<T: Config> = StorageValue<_, Vec<Dorg<T::AccountId>>>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn votes)]
+    pub type Votes<T: Config> = StorageDoubleMap<
+        _,
+        Blake2_256,
+        Dorg<T::AccountId>,
+        Blake2_256,
+        CallHash,
+        u32
+    >;
+
+    #[pallet::storage]
+    #[pallet::getter(fn users_votes)]
+    pub type UsersVotes<T: Config> = StorageDoubleMap<
+        _,
+        Blake2_256,
+        (Dorg<T::AccountId>, CallHash),
+        Blake2_256,
+        T::AccountId,
+        bool,
+    >;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -57,7 +88,6 @@ pub mod pallet {
 		SomethingStored(u32, T::AccountId),
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		/// Error names should be descriptive.
@@ -66,49 +96,12 @@ pub mod pallet {
 		StorageOverflow,
 	}
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
 			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
 			Ok(().into())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(().into())
-				},
-			}
 		}
 	}
 }
