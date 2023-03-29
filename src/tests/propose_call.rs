ab352fa646290525b45f552c012bf1553e6325e0
decentration <ramseyajram@me.com>
2023-03-29 20:27:42 +0100

    unit tests for Limit Number of Live Proposals

--- src/tests/propose_call.rs
+++ src/tests/propose_call.rs
@@ -244,3 +244,234 @@ fn test_active_proposals_decrement_on_approve() {
 		});
 }
 
+#[test]
+	fn test_propose_call_max_active_proposals() {
+		ExtBuilder::default()
+		.balances(vec![])
+		.build()
+		.execute_with(|| {
+			// create supersig account with BOB
+			assert_ok!(Supersig::create_supersig(
+				RawOrigin::Signed(ALICE()).into(),
+				vec! {
+					(ALICE(), Role::Standard),
+				}
+				.try_into()
+				.unwrap()
+			));
+			let supersig_account = get_supersig_account(0);
+			let call: RuntimeCall = frame_system::Call::remark {
+						remark: "test".into(),
+			}
+			.into();
+			// propose 3 calls
+			for i in 1..3 {
+					Supersig::propose_call(
+						RawOrigin::Signed(ALICE()).into(), 
+						supersig_account.clone(), 
+						Box::new(call.clone()),
+					);
+			};
+
+				// propose a 4th call once one of the active ones is approved
+				assert_ok!(Supersig::approve_call(
+					RawOrigin::Signed(ALICE()).into(), 
+					supersig_account.clone(), 
+					0
+				));
+				assert_ok!(Supersig::propose_call(
+					RawOrigin::Signed(ALICE()).into(), 
+					supersig_account.clone(), 
+					Box::new(call.clone()),
+				));
+			});
+	}
+
+	#[test]
+	fn test_propose_call_active_proposals_multiple_accounts() {
+		ExtBuilder::default()
+			.balances(vec![])
+			.build()
+			.execute_with(|| {
+				// create supersig account with ALICE
+				assert_ok!(Supersig::create_supersig(
+					RawOrigin::Signed(ALICE()).into(),
+					vec! {
+						(ALICE(), Role::Standard),
+						(BOB(), Role::Standard),
+					}
+					.try_into()
+					.unwrap()
+				));
+				assert_ok!(Supersig::create_supersig(
+					RawOrigin::Signed(ALICE()).into(),
+					vec! {
+						(ALICE(), Role::Standard),
+						(BOB(), Role::Standard),
+					}
+					.try_into()
+					.unwrap()
+				));
+				assert_ok!(Supersig::create_supersig(
+					RawOrigin::Signed(ALICE()).into(),
+					vec! {
+						(ALICE(), Role::Standard),
+						(BOB(), Role::Standard),
+					}
+					.try_into()
+					.unwrap()
+				));
+				assert_ok!(Supersig::create_supersig(
+					RawOrigin::Signed(ALICE()).into(),
+					vec! {
+						(ALICE(), Role::Standard),
+						(BOB(), Role::Standard),
+					}
+					.try_into()
+					.unwrap()
+				));
+				assert_ok!(Supersig::create_supersig(
+					RawOrigin::Signed(ALICE()).into(),
+					vec! {
+						(ALICE(), Role::Standard),
+						(BOB(), Role::Standard),
+					}
+					.try_into()
+					.unwrap()
+				));
+				let supersig_account = get_supersig_account(0);
+				let supersig_account_1 = get_supersig_account(1);
+				let supersig_account_2 = get_supersig_account(2);
+				let supersig_account_3 = get_supersig_account(3);
+				let supersig_account_4 = get_supersig_account(4);
+
+				let call: RuntimeCall = frame_system::Call::remark {
+							remark: "test".into(),
+				}
+				.into();
+
+				// propose a call with each account
+				assert_ok!(Supersig::propose_call(
+					RawOrigin::Signed(ALICE()).into(), 
+					supersig_account.clone(), 
+					Box::new(call.clone()),
+					));
+				assert_ok!(Supersig::propose_call(
+					RawOrigin::Signed(BOB()).into(), 
+					supersig_account_1.clone(), 
+					Box::new(call.clone()),
+				));
+				assert_ok!(Supersig::propose_call(
+					RawOrigin::Signed(BOB()).into(), 
+					supersig_account_2.clone(), 
+					Box::new(call.clone()),
+				));
+				assert_ok!(Supersig::propose_call(
+					RawOrigin::Signed(BOB()).into(), 
+					supersig_account_3.clone(), 
+					Box::new(call.clone()),
+				));
+
+				// try to propose a fifth call just to show that MaxCallPerAccount is per account not the total chain.
+				assert_ok!(
+					Supersig::propose_call(
+						RawOrigin::Signed(ALICE()).into(), 
+						supersig_account_4.clone(), 
+						Box::new(call.clone()),
+					)
+				);
+		}
+	);
+}
+#[test]
+	fn test_remove_call_active_proposals() {
+		ExtBuilder::default()
+			.balances(vec![])
+			.build()
+			.execute_with(|| {
+				// create supersig account with ALICE
+				assert_ok!(Supersig::create_supersig(
+					RawOrigin::Signed(ALICE()).into(),
+					vec! {
+						(ALICE(), Role::Standard),
+						(BOB(), Role::Standard),
+					}
+					.try_into()
+					.unwrap()
+				));
+			
+
+				let supersig_account = get_supersig_account(0);
+				let supersig_id = Supersig::get_supersig_id_from_account(&supersig_account).unwrap();
+				let call: RuntimeCall = frame_system::Call::remark {
+					remark: "test".into(),
+		}
+		.into();
+
+		// Test that we cannot propose more than `MaxCallsPerAccount`
+		for i in 1..=3 {
+			Supersig::propose_call(
+				RawOrigin::Signed(ALICE()).into(), 
+				supersig_account.clone(), 
+				Box::new(call.clone()),
+			);
+			// assert_noop!(
+			// 	Supersig::propose_call(
+			// 		RawOrigin::Signed(ALICE()).into(),
+			// 		supersig_account.clone(), 
+			// 		Box::new(call.clone()),
+			// 	),
+			// 	Error::<Test>::TooManyActiveProposals
+			// );
+		}
+		
+		
+
+			// Test that we can propose more after we remove one
+			assert_ok!(Supersig::remove_call(
+				RawOrigin::Signed(ALICE()).into(), 
+				supersig_account.clone(), 
+				0
+			));
+			assert_eq!(Supersig::active_proposals(
+				supersig_id.clone(),
+			),
+				2
+			);
+
+			assert_ok!(Supersig::propose_call(
+				RawOrigin::Signed(ALICE()).into(), 
+					supersig_account.clone(), 
+					Box::new(call.clone()),
+			));
+			assert_eq!(Supersig::active_proposals(
+				supersig_id.clone(),
+				),
+				3
+			);
+
+			// Test that we cannot remove more than `MaxCallsPerAccount` calls
+			for i in 1..=3 {
+				assert_ok!(Supersig::remove_call(
+					RawOrigin::Signed(ALICE()).into(), 
+					supersig_account.clone(), 
+					i,
+				));
+				assert_noop!(
+					Supersig::remove_call(
+						RawOrigin::Signed(ALICE()).into(), 
+						supersig_account.clone(), 
+						(i as u32 - 1).into(),
+					),
+					Error::<Test>::CallNotFound,
+				);
+			}
+			assert_eq!(
+				Supersig::active_proposals(
+					supersig_id.clone()
+				), 
+			0
+			);
+		}
+	);
+	}
